5b325ddb070e7079d8bf776b5b3e8c920ccc155c
jpnacu17 <jp.nacu17@gmail.com>
2025-11-02 23:50:10 -0700

    ica6
    need to work on javascript

---
+++ icas/ICA06/js/ica6.js
@@ -0,0 +1,16 @@
+
+window.onload = () => {
+
+
+}
+
+function changelocation() {
+    location = "https://www.google.com/";
+}
+function changebackground(this){
+    // getcolor
+    const color = this.color;
+    document.body.style.background=  color;
+
+}
+
