a768dfb4ac5b2054096f41d4c23649092b5845aa
jpnacu17 <jp.nacu17@gmail.com>
2025-11-03 09:05:52 -0700

    ica6 udpates

--- icas/ICA06/js/ica6.js
+++ icas/ICA06/js/ica6.js
@@ -1,16 +1,23 @@
 
 window.onload = () => {
-
+//document.querySelector("#colorbutton").onclick = changebackground();
 
 }
 
 function changelocation() {
     location = "https://www.google.com/";
 }
-function changebackground(this){
+
+function changebackground(btn){
     // getcolor
-    const color = this.color;
-    document.body.style.background=  color;
+    //const color = this.color;
+    
+    document.body.style.background= getElementById(backgroundcolor).value;
+
+    // document.querySelector(btn).style.backgroundColor =
+    // document.body.style.background = document.querySelector("#backgroundcolor").value;
 
 }
+
+
 
