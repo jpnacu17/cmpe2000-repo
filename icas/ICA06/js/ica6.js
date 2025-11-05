01b4af2cffc7df20487dc46ca44766b1b7a60104
jpnacu17 <jp.nacu17@gmail.com>
2025-11-05 16:12:32 -0700

    ica 6

--- icas/ICA06/js/ica6.js
+++ icas/ICA06/js/ica6.js
@@ -2,7 +2,9 @@
 window.onload = () => {
 document.querySelector("#colorbutton").onclick = changebackground;
 document.querySelector("#currentdimension").onclick = obtaindimensions;
-document.querySelector("#time").oncick = gettime;
+document.querySelector("#time").onclick = gettime;
+document.querySelector("#agent").onclick = getagent;
+document.querySelector("#prompt").onclick = prompttest;
 
 }
 
@@ -31,7 +33,30 @@ function gettime(){
 
     let time = d.toTimeString();
     
-    window.alert("time is" + time);
+    window.alert("time is: " + time); 
 
 }
+function getagent(){
+    this.innerHTML = navigator.userAgent;
+}
+
+function prompttest(){
+
+    // var userprompt = window.prompt("Current Value" + innerHTML + "Enter a new number");
+  
+    var string = "Current Value" + userprompt + "Enter a new number";
+    var userprompt = window.prompt(string, "5");
+
+
+    // var userprompt = window.prompt("Current Value" + innerHTML + "Enter a new number");
+
+    // if (isNaN(userpompt)){
+    //     console.log(innerHTML + "is Null or NaN")
+    // }
+    // else
+    // document.querySelector("#prompt").innerHTML = userprompt;
+
+
+}
+
 
