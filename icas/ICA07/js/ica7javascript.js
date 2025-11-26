5fbabf450c8e639d4d759afc8b3e7df49309dda4
jpnacu17 <jp.nacu17@gmail.com>
2025-11-26 15:51:29 -0700

    ica7

--- icas/ICA07/js/ica7javascript.js
+++ icas/ICA07/js/ica7javascript.js
@@ -3,11 +3,26 @@
 
 
 onload = ()=> {
-
+document.querySelector("#purchase").onlick = UpdateMake;
 
 }
+//step 1
+function UpdateMake(){
+    //uncheck all model radio buttons 
+    //use queryseel.ector all
+    let radio = document.querySelectorAll(".makeclass");
+    radio.checked = false; //should hopefullt uncheck the buttons
 
-function validation(){
-    let make = document.querySelectorAll('.makeclass");
-    console.log()
+    
 }
+function UpdateStatus(){
+//invoke within UDpateMake()
+// display input into text at the bottom of the webpage
+}
+
+// function validation(){
+//     let make = document.querySelectorAll(".makeclass");
+//     console.log(make);
+
+
+// }
